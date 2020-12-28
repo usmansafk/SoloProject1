2b8ce2b95b9053da8bb36487dda44ac4f1783b8a
Usman Sajid <67691352+code190715@users.noreply.github.com>
2020-12-28 20:44:41 +0000

    Add files via upload

---
+++ MyFirstSoloAttempt.java
@@ -0,0 +1,14 @@
+public class MyFirstSoloAttempt {
+	
+	public static void main (String[] args) {
+		int x = 2;
+		int y = 5;
+		String firstDigit = "1";
+
+		x = x * y;
+		System.out.println("x is now " + x);
+
+
+	}
+
+}
